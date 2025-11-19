f47aeddd0c87a5da566e79a437af8d0e19bfa84e
Mwangi in cloud <cronosm4m@gmail.com>
2025-11-19 10:51:45 +0300

    Update variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "instance_count" {
+    type = number
+    description = "*"
+}
+
+variable "instance_ami" {
+    description = "*"
+    type = string 
+}
+
+variable "instance_name" {
+    description = "*"
+    type = string
+}
+
+variable "instance_type" {
+    description = "*"
+    type = string 
+}
+variable "key_name" {
+    description = "*"
+    type = string
+}
